cf3e3cbc63fca6cfe4798153a061de1c8aa4bc9f
victor gabriel R <victorgrs11@outlook.com>
2024-06-03 20:15:21 -0300

    (feat) home desing

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,10 @@
 import savingsLogo from '../img/savings.svg'
 import Button from '../layout/Button'
+import styles from './Home.module.css'
 
 function Home() {
   return(
-    <section>
+    <section className={styles.home_container}>
       <h1>Bem vindo ao <span>costs!</span></h1>
       <p>faca seu projeto</p>
       <Button to='#' name='criar projeto'/>
