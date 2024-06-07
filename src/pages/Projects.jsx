d6ee73aa62c68eff6b54b5d0c42da1984d432308
victor gabriel R <victorgrs11@outlook.com>
2024-06-07 20:47:37 -0300

    (feat) adding function in projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,48 @@ import Conteiner from "../layout/Container"
 import { useLocation } from "react-router-dom"
 
 function Projects() {
+  
+  const [projects, setProjects] = useState([])
+  const [RemoveLoading, setRemoveLoading] = useState(false)
+  const [MessageProject, setMessageProject] = useState('')
+  
+  const location = useLocation()
 
+  let message = ''
+  if(location.state){
+    message= location.state.message
+  }
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:5000/projects', {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json',}
+      })
+      .then((resp) => resp.json())
+      .then((dados) => {
+        setProjects(dados)
+        setMessageProject(true)
+      })
+      .catch((err) => console.log(err))
+    }, 1000)
+  },[])
+
+
+  const removeProject = (id) => {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: 'DELETE',
+      headers: {'Content-Type' : 'application/json'}
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setProjects(projects.filter((project) => project.id !== id))
+      setMessageProject('Projeto deletado com sucesso!')
+    })
+    .catch((err) => console.log(err))
+    
+  }
 
   return(
     <div>
